9d93693620d3c84af71c78ef492db95753190e96
VeeranganaMalav <veeranganamalav01@gmail.com>
2022-12-29 22:56:56 +0530

    Day 4 Problems

---
+++ Conditional Statements/conditional_iw4.js
@@ -0,0 +1,12 @@
+let givenUsername = "veera";
+let givenPassword = "1234";
+
+let inputUsername = "veera";
+let inputPassword = "1234";
+
+if(givenUsername == inputUsername && givenPassword == inputPassword){
+  console.log("user can login");
+}
+else{
+  console.log("user cannot login");
+}
